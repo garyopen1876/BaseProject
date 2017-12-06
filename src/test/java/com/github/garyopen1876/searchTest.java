aeb6abe50cdcf865d47994c2d1159eb9983b3106
garyopen1876 <garyopen1876>
2017-12-06 12:12:49 +0800

    程式碼

---
+++ src/test/java/com/github/garyopen1876/searchTest.java
@@ -0,0 +1,16 @@
+package com.github.garyopen1876;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class searchTest {
+    search s=new search(5,10,5);
+    @Test
+    public void getType() throws Exception {
+    }
+    public void test() {
+        assertEquals(s,"不是三角形");
+    }
+
+}
