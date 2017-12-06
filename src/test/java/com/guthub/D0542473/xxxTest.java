91c8a0fc0cc9ad317857303cfe21fd10c2c68636
D0542473 <j90297@gm.sysh.tc.edu.tw>
2017-12-06 12:35:20 +0800

    教學課程

---
+++ src/test/java/com/guthub/D0542473/xxxTest.java
@@ -0,0 +1,12 @@
+package com.guthub.D0542473;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class xxxTest {
+    @Test
+    public void test1() throws Exception {
+        assertEquals("test OK", new xxx().name(1));
+    }
+}
