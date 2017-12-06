ce86743f60d8527d49e3a4c72380587c1ffabf07
D0542473 <j90297@gm.sysh.tc.edu.tw>
2017-12-06 12:31:14 +0800

    教學課程

---
+++ src/main/java/com/guthub/D0542473/xxx.java
@@ -0,0 +1,13 @@
+package com.guthub.D0542473;
+
+import java.util.Scanner;
+
+public class xxx {
+    public String name(int number){
+        if(number == 1){
+            return "illya";
+        }  else{
+            return "lin";
+        }
+    }
+}
